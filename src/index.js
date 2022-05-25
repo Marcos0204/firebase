bd997a1129adda6478938a2b3c4ab0b582f30077
Marcos Bastidas <marcosbastidas301@gmail.com>
2022-05-25 16:10:46 +0200

    redux-config_initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';  // Importamos React
 import ReactDOM from 'react-dom'; // Importamos ReactDOM
+import { Provider } from 'react-redux'
 import App from './App'; // Importamos nuestro primer componente
+import { store } from './store/store'
 
 // insertamos nuestro componente en nuestro documento HTML utilizando ReactDOM
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
